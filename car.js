515f61aad60769d1c577b8c210edcc1bcc179397
Denis <165719269+Zloymod@users.noreply.github.com>
2024-07-23 22:22:21 +0300

    Add files via upload

---
+++ car.js
@@ -0,0 +1,21 @@
+const carName = "mazda rx-7";
+let maxCarSpeed = "280 км/ч";
+let carOwner = "Денис";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 295;
+console.log("У машины изменилась скорость", maxCarSpeed);
+maxCarSpeed = 270;
+console.log("У машины изменилась скорость", maxCarSpeed);
+maxCarSpeed = 265;
+console.log("У машины изменилась скорость", maxCarSpeed);
+// В задании не указано, что цыфры без ковычек можно указывать в изменение переменной, а вот слова нужно обязательно в ковычки :) и в учебном тоже нету такой инфы.
+carOwner = "Кирилл";
+console.log("У машины изменился владелец", carOwner);
+carOwner = "Илья";
+console.log("У машины изменился владелец", carOwner);
+carOwner = "Дима";
+console.log("У машины изменился владелец", carOwner);
---
+++ index.js
@@ -0,0 +1,13 @@
+// JavaScript-код с созданием переменных
+const myName = "Денис";
+const mySurname = "Королёв";
+let myFavoriteDrink = "Кофе";
+let myFavoriteAnimal = "Кот";
+let myFavoriteProgrammingLanguage = "JavaScript";
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
+/*Автор
+Денис Королёв */
